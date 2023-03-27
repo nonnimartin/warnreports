4ba6bcd96b83054359ff208aae7b94a81b62be27
nonnimartin <nonnimartin@gmail.com>
2023-03-27 16:44:17 -0700

    Now makes lists under comp header

--- reader.py
+++ reader.py
@@ -1,16 +1,15 @@
 import pandas as pd
 import csv
 import openpyxl
+import json
 
 class Reader:                   
     # convert excel file to csv
     def xlsx_to_dict(self, path):
         wb = openpyxl.load_workbook(path)
         worksheet = wb.active
-        csv_dict = dict()
 
         columns_names = list()
-        counter = 0
         sheet_dict = dict()
         for row in worksheet:
             if len(columns_names) == 0:
@@ -18,15 +17,24 @@ class Reader:
                 for cell in row:
                     columns_names.append(cell.value)
             else:
-                # populate 
+                # populate
                 this_dict = dict()
                 for header in columns_names:
                     this_index = columns_names.index(header)
                     this_dict[header] = row[this_index].value
-                sheet_dict[counter] = this_dict
-                counter += 1
+                if this_dict["Company"] not in sheet_dict.keys():
+                    company_name = this_dict["Company"]
+                    new_list = list()
+                    new_list.append(this_dict)
+                    sheet_dict[company_name] = new_list
+                elif this_dict["Company"] in sheet_dict.keys():
+                    company_name = this_dict["Company"]
+                    current_list = sheet_dict[company_name]
+                    current_list.append(this_dict)
+                    sheet_dict[company_name] = current_list
 
         return sheet_dict
     
 reader = Reader()
-print(reader.xlsx_to_dict("ca_warn_data.xlsx"))
+#json.dumps(reader.xlsx_to_dict("ca_warn_data.xlsx"), default=str)
+print(json.dumps(reader.xlsx_to_dict("ca_warn_data.xlsx"), default=str))
