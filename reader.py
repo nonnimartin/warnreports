ba24f0ad353d9ebe594a653dcb92dbcb904ba5b5
nonnimartin <nonnimartin@gmail.com>
2023-03-29 18:56:44 -0700

    Beginning work on writer class

--- reader.py
+++ reader.py
@@ -44,8 +44,36 @@ class Reader:
             print(soup.find_all('table'))
             # return soup
 
+    def format_data_from_xlsx(self, paths_list):
+        this_reader = Reader()
+        return_dict = dict()
+        # for list of paths, format data into fields ready for rss
+        
+        xlsx_dict = this_reader.xlsx_to_dict(os.path.expanduser('~') + '/.warn-scraper/cache/ca/warn_report.xlsx')
+        
+        # test with one case
+        for company in xlsx_dict:
+            # ignore numbered headers and loop through companies
+            if not str(company).isnumeric():
+                # list of notices for "items" bit of rss doc
+                items_list = list()
+                # loop through company's notices
+                for notice in xlsx_dict[company]:
+                    notice_dict = dict()
+                    this_company = notice['Company']
+                    layoff_or_closure = notice['Layoff/\nClosure']
+                    number_employees = notice['No. Of\nEmployees']
+                    effective_date = str(notice['Notice\nDate'])
+                    date_received = str(notice['Received\nDate'])
+                    this_info = this_company + ' - ' + layoff_or_closure + ' - ' + 'No. of Employees: ' + str(number_employees) + ' - Effective Date: ' + effective_date + ' - Date Received: ' + date_received
+                    notice_dict['title'] = this_company
+                    notice_dict['description'] = "See below items"
+                    print(notice_dict)
+
+
 reader = Reader()
+reader.format_data_from_xlsx(list())
 #json.dumps(reader.xlsx_to_dict("ca_warn_data.xlsx"), default=str)
 #print(json.dumps(reader.xlsx_to_dict("ca_warn_data.xlsx"), default=str))
-print(reader.xlsx_to_dict(os.path.expanduser('~') + '/.warn-scraper/cache/ca/warn_report.xlsx'))
+#print(reader.xlsx_to_dict(os.path.expanduser('~') + '/.warn-scraper/cache/ca/warn_report.xlsx'))
 #print(reader.html_to_dict("./test_wa.html"))
---
+++ writer.py
@@ -0,0 +1,11 @@
+from random import SystemRandom
+import datetime
+from rfeed import *
+
+class Writer(self, items_list):                   
+    # write rss data from list of WARN notice items
+
+    for item in items_list:
+        # write guid for RSS object
+        random = SystemRandom()
+        
