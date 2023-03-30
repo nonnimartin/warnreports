a1bc52baea04be9296193fb7016f4f673a1b68a4
nonnimartin <nonnimartin@gmail.com>
2023-03-29 19:13:38 -0700

    Reader formatting correctly, but has test case hardcoded

--- reader.py
+++ reader.py
@@ -57,9 +57,9 @@ class Reader:
             if not str(company).isnumeric():
                 # list of notices for "items" bit of rss doc
                 items_list = list()
+                notice_dict = dict()
                 # loop through company's notices
                 for notice in xlsx_dict[company]:
-                    notice_dict = dict()
                     this_company = notice['Company']
                     layoff_or_closure = notice['Layoff/\nClosure']
                     number_employees = notice['No. Of\nEmployees']
@@ -67,8 +67,19 @@ class Reader:
                     date_received = str(notice['Received\nDate'])
                     this_info = this_company + ' - ' + layoff_or_closure + ' - ' + 'No. of Employees: ' + str(number_employees) + ' - Effective Date: ' + effective_date + ' - Date Received: ' + date_received
                     notice_dict['title'] = this_company
-                    notice_dict['description'] = "See below items"
-                    print(notice_dict)
+                    
+                    
+                    this_item = dict()
+                    this_item['title'] = company
+                    this_item['description'] = this_info
+                    items_list.append(this_item)
+                
+                notice_dict['title'] = company
+                notice_dict['description'] = 'See below list of notices'
+                notice_dict['items'] = items_list
+                return_dict[company] = notice_dict
+        print(json.dumps(return_dict))
+                
 
 
 reader = Reader()
