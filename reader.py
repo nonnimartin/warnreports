72e64018a36519eb1e04279fd14cf658d2e2df62
nonnimartin <nonnimartin@gmail.com>
2023-05-23 11:34:36 -0700

    All available states now parsed

--- reader.py
+++ reader.py
@@ -44,7 +44,8 @@ class Reader:
             "notice_date" : "Initial Report Date",
             "city" : "City",
             "number_of_employees_affected" : "Planned # Affected Employees",
-            "Planned Starting Date" : "No starting date specified"
+            "Planned Starting Date" : "No starting date specified",
+            "warn_type" : "Closing or Layoff"
         }
 
         conversion_dict_al = {
@@ -56,10 +57,198 @@ class Reader:
             "Planned Starting Date" : "Planned Starting Date"
         }
 
+        conversion_dict_ca = {
+            "company" : "Company",
+            "notice_date" : "Initial Report Date",
+            "city" : "City",
+            "num_employees" : "Planned # Affected Employees",
+            "layoff_or_closure" : "Closing or Layoff",
+            "effective_date" : "Planned Starting Date"
+        }
+
+        conversion_dict_co = {
+            "company" : "Company",
+            "received_date" : "Initial Report Date",
+            "city" : "City",
+            "jobs" : "Planned # Affected Employees",
+            "occupations" : "Closing or Layoff",
+            "begin_date" : "Planned Starting Date"
+        }
+
+        conversion_dict_dc = {
+            "Organization Name" : "Company",
+            "Notice Date" : "Initial Report Date",
+            "city" : "City",
+            "Number toEmployees Affected" : "Planned # Affected Employees",
+            "layoff_or_closure" : "Closing or Layoff",
+            "begin_date" : "Planned Starting Date"
+        }
+
+        conversion_dict_de = {
+            "employer" : "Company",
+            "Notice Date" : "Initial Report Date",
+            "City" : "City",
+            "number_of_employees_affected" : "Planned # Affected Employees",
+            "warn_type" : "Closing or Layoff"
+        }
+
+        conversion_dict_ia = {
+            "Company" : "Company",
+            "Notice Date" : "Initial Report Date",
+            "City" : "City",
+            "Emp #" : "Planned # Affected Employees",
+            "Notice Type" : "Closing or Layoff",
+            "Layoff Date" : "Planned Starting Date"
+        }
+
+        conversion_dict_in = {
+            "Company" : "Company",
+            "Notice Date" : "Initial Report Date",
+            "City" : "City",
+            "Affected Workers" : "Planned # Affected Employees",
+            "Notice Type" : "Closing or Layoff",
+            "LO/CL Date" : "Planned Starting Date"
+        }
+
+        conversion_dict_ks = {
+            "employer" : "Company",
+            "notice_date" : "Initial Report Date",
+            "city" : "City",
+            "number_of_employees_affected" : "Planned # Affected Employees",
+            "warn_type" : "Closing or Layoff",
+            "LO/CL Date" : "Planned Starting Date"
+        }
+
+        conversion_dict_md = {
+            "Company" : "Company",
+            "Notice Date" : "Initial Report Date",
+            "Location" : "City",
+            "Total Employees" : "Planned # Affected Employees",
+            "Type" : "Closing or Layoff",
+            "Effective Date" : "Planned Starting Date"
+        }
+
+        conversion_dict_me = {
+            "employer" : "Company",
+            "notice_date" : "Initial Report Date",
+            "city" : "City",
+            "number_of_employees_affected" : "Planned # Affected Employees",
+            "warn_type" : "Closing or Layoff",
+            "Effective Date" : "Planned Starting Date"
+        }
+
+        conversion_dict_mo = {
+            "Title" : "Company",
+            "Received Sort descending" : "Initial Report Date",
+            "Location(s)" : "City",
+            "# affected" : "Planned # Affected Employees",
+            "Type" : "Closing or Layoff",
+            "Layoff date(s)" : "Planned Starting Date"
+        }
+
+        conversion_dict_ny = {
+            "company_name" : "Company",
+            "date_posted" : "Initial Report Date",
+            "City" : "City",
+            "Number Affected" : "Planned # Affected Employees",
+            "Dislocation Type" : "Closing or Layoff",
+            "notice_dated" : "Planned Starting Date"
+        }
+
+        conversion_dict_ok = {
+            "employer" : "Company",
+            "date_posted" : "Initial Report Date",
+            "city" : "City",
+            "number_of_employees_affected" : "Planned # Affected Employees",
+            "warn_type" : "Closing or Layoff",
+            "notice_date" : "Planned Starting Date"
+        }
+
+        conversion_dict_or = {
+            "Company Name" : "Company",
+            "Received Date" : "Initial Report Date",
+            "Location" : "City",
+            "Laid Off" : "Planned # Affected Employees",
+            "Layoff Type" : "Closing or Layoff",
+            "Layoff Date" : "Planned Starting Date"
+        }
+
+        conversion_dict_tx = {
+            "JOB_SITE_NAME" : "Company",
+            "NOTICE_DATE" : "Initial Report Date",
+            "CITY_NAME" : "City",
+            "TOTAL_LAYOFF_NUMBER" : "Planned # Affected Employees",
+            "Layoff Type" : "Closing or Layoff",
+            "Layoff Date" : "Planned Starting Date"
+        }
+
+        conversion_dict_ut = {
+            "Company Name" : "Company",
+            "Date of Notice" : "Initial Report Date",
+            "Location" : "City",
+            "Affected Workers" : "Planned # Affected Employees",
+            "Layoff Type" : "Closing or Layoff",
+            "Layoff Date" : "Planned Starting Date"
+        }
+
+        conversion_dict_va = {
+            "Company Name" : "Company",
+            "Notice Date" : "Initial Report Date",
+            "Location City" : "City",
+            "Employees Affected" : "Planned # Affected Employees",
+            "Layoff Type" : "Closing or Layoff",
+            "Impact Date" : "Planned Starting Date"
+        }
+
+        conversion_dict_vt = {
+            "employer" : "Company",
+            "notice_date" : "Initial Report Date",
+            "city" : "City",
+            "number_of_employees_affected" : "Planned # Affected Employees",
+            "warn_type" : "Closing or Layoff",
+            "Impact Date" : "Planned Starting Date"
+        }
+
         if state == "az":
             return conversion_dict_az
         elif state == "al":
             return conversion_dict_al
+        elif state == "ca":
+            return conversion_dict_ca
+        elif state == "co":
+            return conversion_dict_co
+        elif state == "dc":
+            return conversion_dict_dc
+        elif state == "de":
+            return conversion_dict_de
+        elif state == "ia":
+            return conversion_dict_ia
+        elif state == "in":
+            return conversion_dict_in
+        elif state == "ks":
+            return conversion_dict_ks
+        elif state == "md":
+            return conversion_dict_md
+        elif state == "me":
+            return conversion_dict_me
+        elif state == "mo":
+            return conversion_dict_me
+        elif state == "ny":
+            return conversion_dict_ny
+        elif state == "ok":
+            return conversion_dict_ok
+        elif state == "or":
+            return conversion_dict_or
+        elif state == "sc":
+            return conversion_dict_sc
+        elif state == "tx":
+            return conversion_dict_tx
+        elif state == "ut":
+            return conversion_dict_ut
+        elif state == "va":
+            return conversion_dict_va
+        elif state == "vt":
+            return conversion_dict_vt
         else:
             return
     
@@ -83,13 +272,16 @@ class Reader:
                     this_dict = dict()
                     for item in line:
                         header_index = line.index(item)
-                        print(convert_dict)
                         if headers_list[header_index] in convert_dict:
                             this_header = convert_dict[headers_list[header_index]]
                             this_dict[this_header] = item
                         if this_header == "Company":
                             company = item
                     # we will have to put n/a into any fields not included
+                    check_dict = ["City", "Initial Report Date", "Planned # Affected Employees", "Closing or Layoff", "Planned Starting Date"]
+                    for field in check_dict:
+                        if field not in this_dict:
+                            this_dict[field] = "N/A or Not Provided"
                     return_dict[company] = this_dict
         return return_dict
                     
@@ -143,5 +335,5 @@ class Reader:
 
 reader = Reader()
 
-print(json.dumps(reader.csv_to_dict(os.path.expanduser('~') + '/.warn-scraper/exports/az.csv', 'az')))
+print(json.dumps(reader.csv_to_dict(os.path.expanduser('~') + '/.warn-scraper/exports/vt.csv', 'vt')))
 #json.dumps(reader.csv_to_dict(os.path.expanduser('~') + '/.warn-scraper/exports/az.csv', 'az'))
