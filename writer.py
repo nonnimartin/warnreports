b02379297b8b92bfaef5af8d18e2408e996aff32
nonnimartin <nonnimartin@gmail.com>
2023-06-12 18:44:20 -0700

    Removed commented code

--- writer.py
+++ writer.py
@@ -39,40 +39,7 @@ class Writer:
             fe.title = company + " - " + closing_or_layoff + " - " + "Reported: " + initial_reporting_date + " - " + "Starting: " + planned_starting_date + " - " + "City: " + city + " - " + " Employees Affected: " + planned_no_affected_employees
             fe.link(href='http://lernfunk.de/feed')
             fe.enclosure('http://lernfunk.de/media/654321/1/file.mp3', 0, 'audio/mpeg')
-            # for this_item in notices_dict[company]:
-            #     print(this_item)
-            #     this_item = json.loads(this_item)
-            #     closing_or_layoff = this_item["Closing or Layoff"]
-            #     initial_reporting_date = this_item["Initial Report Date"]
-            #     planned_starting_date = this_item["Planned Starting Date"]
-            #     city = this_item["City"]
-            #     planned_no_affected_employees = this_item["Planned # Affected Employees"]
-            #     # write guid for RSS object
-            #     random =  uuid.uuid4()
-            #     fe = fg.add_entry()
-            #     fe.title = this_item[company + " - " + closing_or_layoff + " - " + "Reported:" + initial_reporting_date + " - " + "Starting: " + planned_starting_date + " - " + "City:" + city]
-            #     #fe.description = this_item['description']
-            #     #fe.id(random)
-            #     fe.link(href='http://lernfunk.de/feed')
-            #     fe.enclosure('http://lernfunk.de/media/654321/1/file.mp3', 0, 'audio/mpeg')
-            #     # feed = rf.Feed(notices_dict[company]['title'], )
-            #     #items_list.append(itemified)
-            
-            #print(fg.rss_file("test.xml"))
-            #fg.rss_str(pretty=True)
-            #fg.rss_file('/tmp/test.json')
-
-            # list of rfeed Item objects from array in dict
-            # items_list = list()
-            # for this_item in notices_dict[company]['items']:
-            #     #print(this_item)
-            #     itemified = rf.Item(this_item['title'], 'www.google.com', this_item['description'], 'nonnimartin@gmail.com', 'warn notice', None, None, None, None)
-            #     # feed = rf.Feed(notices_dict[company]['title'], )
-            #     items_list.append(itemified)
-            
-            # print(rf.Feed(company, 'www.google.com', 'See notices below', 'en-US', None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, items_list).rss())
-    
-
+           
     def store_contact(self, email, company, state):
         try: 
             con = sqlite3.connect("warnDb.db")
