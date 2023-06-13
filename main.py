e770a99fd47e613b331b235f8dabb494aba76438
nonnimartin <nonnimartin@gmail.com>
2023-06-12 17:20:56 -0700

    Updated endpoint

--- main.py
+++ main.py
@@ -3,9 +3,7 @@ import writer
 
 app = FastAPI()
 
-@app.get("/")
+@app.get("/all_companies")
 def read_root():
     this_writer = writer.Writer()
     return this_writer.get_all_companies()
-
-    # return writer.all_companies
