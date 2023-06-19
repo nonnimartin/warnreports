d75789326e8f1be06c436f08802fe1f39d10b490
nonnimartin <nonnimartin@gmail.com>
2023-06-19 10:52:20 -0700

    Added additional fields to store_contact method

--- writer.py
+++ writer.py
@@ -3,48 +3,19 @@ from feedgen.feed import FeedGenerator
 import json
 import sqlite3
 import logging
+import time
 
 class Writer:
-
-    # write rss data from list of WARN notice items
-    feed_list = list()
-
-    def write_rss(self, notices_dict):
-        counter = 0
-        # write guid for RSS object
-        random =  uuid.uuid4()
-        fg = FeedGenerator()
-        fg.id("random")
-        fg.title("WARN NOTICE")
-        fg.author( {'name':'John Doe','email':'john@example.kitchen'} )
-        fg.link( href='http://larskiesow.de/test.atom', rel='self' )
-        fg.language('en')
-        fg.description('See below items')
-        for company in notices_dict.keys():
-
-
-            info = notices_dict[company]
-            closing_or_layoff = info["Closing or Layoff"]
-            initial_reporting_date = info["Initial Report Date"]
-            if "Planned Starting Date" in info:
-                planned_starting_date = info["Planned Starting Date"]
-            city = info["City"]
-            planned_no_affected_employees = info["Planned # Affected Employees"]
-            random =  uuid.uuid4()
-            fe = fg.add_entry()
-
-            fe.id("entry " + str(counter))
-            counter +=1 
-            fe.description(company + " - " + closing_or_layoff + " - " + "Reported: " + initial_reporting_date + " - " + "Starting: " + planned_starting_date + " - " + "City: " + city + " - " + " Employees Affected: " + planned_no_affected_employees)
-            fe.title = company + " - " + closing_or_layoff + " - " + "Reported: " + initial_reporting_date + " - " + "Starting: " + planned_starting_date + " - " + "City: " + city + " - " + " Employees Affected: " + planned_no_affected_employees
-            fe.link(href='http://lernfunk.de/feed')
-            fe.enclosure('http://lernfunk.de/media/654321/1/file.mp3', 0, 'audio/mpeg')
            
     def store_contact(self, email, company, state):
         try: 
             con = sqlite3.connect("warnDb.db")
             cur = con.cursor()
-            cur.execute('INSERT INTO contacts VALUES (?, ?, ?)', (email, company, state))
+            # add unit timestamp for created
+            # set 0 for confirmed
+            # set 0 for last notice
+            now = int(time.time())
+            cur.execute('INSERT INTO contacts VALUES (?, ?, ?, ?, ?, ?)', (email, company, state, 0, now, 0))
             con.commit()
         except:
             logging.exception("Error writing to contacts")
