dd47510024278413d93af96543f894a7ff573eee
Doug Owings <owings1@gmail.com>
2024-05-30 21:22:19 -0700

    bump warn-scraper ver

--- scripts/install-warn-scraper.sh
+++ scripts/install-warn-scraper.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 basedir="$(dirname "$0")/.."
-version=1.2.72
+version=1.2.73
 url="https://github.com/biglocalnews/warn-scraper/archive/refs/tags/$version.tar.gz"
 cd "$basedir"
 curl -fsL "$url" | tar xz --strip-components 1 "warn-scraper-$version/warn"
