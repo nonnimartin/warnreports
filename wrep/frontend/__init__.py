8bd317b8783932267002d2fbcc2e2da444163caa
Doug Owings <owings1@gmail.com>
2025-02-05 21:11:09 -0800

    cleanup build

--- wrep/frontend/__init__.py
+++ wrep/frontend/__init__.py
@@ -2,63 +2,72 @@ from __future__ import annotations
 
 import glob
 import shutil
+from typing import Iterator
 
 from .. import settings, utils
 
 logger = utils.get_logger('frontend')
 
 class FrontentBuilder:
-    def __init__(self):
-        import jinja2
-        import sass
 
-        from ..routers.frontend import routes
-        self.routepaths = set(routes.values())
+    def __init__(self) -> None:
+        import jinja2
         self.src = settings.FRONTEND_SRC
         self.dist = settings.FRONTEND_DIST
         self.jinja = jinja2.Environment(loader=jinja2.FileSystemLoader(self.src))
-        self.scss_context = dict(bootstrap_dir=settings.BOOTSTRAP_DIR)
-        self.sass = sass
 
-    async def build(self):
+    async def build(self) -> None:
         await self.clean()
+        await self.init()
+        await self.copy_assets()
+        await self.build_html()
+        await self.build_scss()
+
+    async def clean(self) -> None:
+        if self.dist.exists():
+            shutil.rmtree(self.dist)
+
+    async def init(self) -> None:
         self.dist.mkdir(parents=True, exist_ok=True)
-        htmltmpl = self.jinja.get_template('frontend.jinja')
+
+    async def copy_assets(self) -> None:
         for path in self.glob('**/*.js', '**/*.css'):
             dest = self.dist/'assets'/path
             dest.parent.mkdir(parents=True, exist_ok=True)
             shutil.copyfile(self.src/path, dest)
-            if path.endswith('.js'):
-                routepath = '/' + path.removesuffix('.js').removeprefix('js/')
-                if routepath in self.routepaths:
-                    htmlpath = f'{routepath[1:]}.html'
-                    logger.info(f'building {htmlpath}')
-                    htmldest = self.dist/htmlpath
-                    htmldest.parent.mkdir(parents=True, exist_ok=True)
-                    htmldest.write_text(htmltmpl.render(path=routepath))
+
+    async def build_html(self) -> None:
+        from ..routers.frontend import routes
+        htmltmpl = self.jinja.get_template('frontend.jinja')
+        for routepath in dict.fromkeys(routes.values()):
+            htmlpath = f'{routepath[1:]}.html'
+            logger.info(f'building {htmlpath}')
+            htmldest = self.dist/htmlpath
+            htmldest.parent.mkdir(parents=True, exist_ok=True)
+            htmldest.write_text(htmltmpl.render(path=routepath))
+
+    async def build_scss(self) -> None:
+        import sass
+        context = dict(bootstrap_dir=settings.BOOTSTRAP_DIR)
         for path in self.glob('**/*.scss'):
-            logger.info(f'Building {path}')
-            tmpl = self.jinja.get_template(path)
-            content = tmpl.render(self.scss_context)
             base = path.removesuffix('.scss')
+            logger.info(f'Building {base}.css')
+            tmpl = self.jinja.get_template(path)
+            content = tmpl.render(context)
             dest = self.dist/'assets'/f'{base}.css'
             dest.parent.mkdir(parents=True, exist_ok=True)
-            dest.write_text(self.sass.compile(string=content))
+            dest.write_text(sass.compile(string=content))
+            logger.info(f'Building {base}.min.css')
             dest = self.dist/'assets'/f'{base}.min.css'
-            dest.write_text(self.sass.compile(string=content, output_style='compressed'))
+            dest.write_text(sass.compile(string=content, output_style='compressed'))
 
-    async def clean(self):
-        if self.dist.exists():
-            shutil.rmtree(self.dist)
-
-    def glob(self, *globs: str):
+    def glob(self, *globs: str) -> Iterator[str]:
         for pat in globs:
             yield from glob.glob(pat, root_dir=self.src, recursive=True)
 
-async def frontend_build():
+async def frontend_build() -> None:
     'Build frontend web assets'
     await FrontentBuilder().build()
-
 
 class Command(utils.BaseCommand):
     commands = dict(build=utils.FuncCommand(frontend_build))
