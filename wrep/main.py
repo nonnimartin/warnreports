5895c40b488cc1b2879ab50ad59f5a225305d111
Doug Owings <owings1@gmail.com>
2025-02-12 19:54:19 -0800

    cleanup main

--- wrep/main.py
+++ wrep/main.py
@@ -2,90 +2,158 @@ from __future__ import annotations
 
 import ipaddress
 from contextlib import asynccontextmanager
-from types import SimpleNamespace
+from functools import wraps
+from typing import Any, Callable, Coroutine, Iterator, Sequence
 
-import click
-from fastapi import FastAPI, HTTPException, Request, status
+from fastapi import FastAPI, HTTPException, Request, Response, status
 from sentry_sdk import capture_exception
 
 from . import frontend, routers, settings, utils
 from .backends.mongo import MissingControlDoc
 from .models import *
 
+type FNext = Callable[[Request], Coroutine[Any, Any, Response]]
 logger = utils.get_logger('main')
 
 
-async def missing_control_doc(req: Request, exc: MissingControlDoc):
-    logger.exception(f'Missing control', exc_info=exc)
-    capture_exception(exc)
-    raise HTTPException(status.HTTP_503_SERVICE_UNAVAILABLE)
+class Apps:
 
-@asynccontextmanager
-async def _default_lifespan(app: FastAPI):
-    utils.init_logging()
-    yield
+    appslist: Sequence[str] = []
+    opts: dict[str, Any] = dict(proxy_headers=False)
 
-async def set_proxy_client(req: Request, call_next):
-    if (fwd := req.headers.get('x-forwarded-for')):
-        addr = fwd.split(' ')[-1]
-        try:
-            ip, port = addr.rsplit(':', 1)
-            ip = ipaddress.ip_address(ip.strip('[]'))
-            req.scope['client'] = (str(ip), int(port))
-        except:
-            logger.warning(f'Cannot parse x-forwarded-for {addr=}')
-    return await call_next(req)
+    def wapp[T, F: Callable[[T], FastAPI]](wrapped: F, appslist: list = appslist) -> property[FastAPI]:
+        name = wrapped.__name__
+        appslist.append(name)
+        @wraps(wrapped)
+        def wrapper(self: T) -> FastAPI:
+            try:
+                return self.__dict__[name]
+            except KeyError:
+                return self.__dict__.setdefault(name, wrapped(self))
+        return property(wrapper)
 
-def _create_app(**kw):
-    kw = dict(
-        lifespan=_default_lifespan,
-        title='warnreports API',
-        openapi_url='/api/v0/openapi.json',
-        docs_url='/api/docs/swagger',
-        redoc_url='/api/docs/redoc') | kw    
-    app = FastAPI(**kw)
-    if settings.UVICORN_PROXY_HEADERS:
-        app.middleware('http')(set_proxy_client)
-    app.add_exception_handler(MissingControlDoc, missing_control_doc)
-    return app
+    @wapp
+    def frontend(self):
+        app = self.create_app(
+            lifespan=frontend.lifespan,
+            title='warnreports frontend')
+        app.include_router(frontend.router, include_in_schema=False)
+        return app
 
-apps = SimpleNamespace()
-apps.frontend = _create_app(lifespan=frontend.lifespan, title='warnreports frontend')
-apps.frontend.include_router(frontend.router, include_in_schema=False)
-apps.search = _create_app()
-apps.search.include_router(routers.search)
-apps.artifacts = _create_app(title='warnreports artifacts')
-apps.artifacts.include_router(routers.artifacts)
-apps.backend = _create_app()
-apps.backend.include_router(routers.backend)
-apps.app = _create_app(lifespan=frontend.lifespan)
-apps.app.include_router(frontend.router, include_in_schema=False)
-apps.app.include_router(routers.backend)
+    @wapp
+    def search(self):
+        app = self.create_app()
+        app.include_router(routers.search)
+        return app
+    
+    @wapp
+    def artifacts(self):
+        app = self.create_app(title='warnreports artifacts')
+        app.include_router(routers.artifacts)
+        return app
+    
+    @wapp
+    def backend(self):
+        app = self.create_app()
+        app.include_router(routers.backend)
+        return app
+    
+    @wapp
+    def app(self):
+        app = self.create_app(lifespan=frontend.lifespan)
+        app.include_router(frontend.router, include_in_schema=False)
+        app.include_router(routers.backend)
+        return app
 
+    del(wapp)
+    appslist = tuple(appslist)
+
+    def create_app(self, **kw) -> FastAPI:
+        kw = dict(
+            lifespan=self.default_lifespan,
+            title='warnreports API',
+            openapi_url='/api/v0/openapi.json',
+            docs_url='/api/docs/swagger',
+            redoc_url='/api/docs/redoc') | kw    
+        app = FastAPI(**kw)
+        app.middleware('http')(self.set_proxy_client)
+        app.add_exception_handler(MissingControlDoc, self.missing_control_doc)
+        return app
+
+    @asynccontextmanager
+    async def default_lifespan(self, app: FastAPI):
+        utils.init_logging()
+        yield
+
+    async def missing_control_doc(self, req: Request, exc: MissingControlDoc) -> None:
+        logger.exception(f'Missing control', exc_info=exc)
+        capture_exception(exc)
+        raise HTTPException(status.HTTP_503_SERVICE_UNAVAILABLE)
+
+    async def set_proxy_client(self, req: Request, call_next: FNext) -> Response:
+        if self.opts['proxy_headers'] and (fwd := req.headers.get('x-forwarded-for')):
+            try:
+                addr = fwd.rsplit(None, 1)[-1]
+                ip, port = addr.rsplit(':', 1)
+                ip = ipaddress.ip_address(ip.strip('[]'))
+                req.scope['client'] = (str(ip), int(port))
+            except:
+                logger.warning(f'Cannot parse x-forwarded-for {fwd=}')
+        return await call_next(req)
+
+apps = Apps()
+
+del(Apps)
+
+class Command:
+
+    def __init__(self) -> None:
+        import click
+        import uvicorn
+        self.pkgname = settings.BASEDIR.name
+        self.delegate = uvicorn.main
+        self.run = click.Command(
+            name='main',
+            callback=self,
+            params=[
+                click.Argument(
+                    ['role'],
+                    default='app',
+                    callback=self.roleopt,
+                    envvar='UVICORN_ROLE'),
+                *self.delegate.params[1:]],
+            context_settings=self.delegate.context_settings)
+
+    def __call__(self, /, *, role: str, **kw) -> None:
+        logger.info(f'Starting uvicorn {role=}')
+        apps.opts['proxy_headers'] = kw['proxy_headers']
+        if kw['reload']:
+            kw['reload_dirs'] = (
+                *kw['reload_dirs'],
+                str(settings.BASEDIR))
+            kw['reload_includes'] = (
+                *kw['reload_includes'],
+                *self.reload_extra(role))
+        app = f'{self.pkgname}.main:apps.{role}'
+        self.delegate.callback(app, **kw)
+
+    @classmethod
+    def main(cls):
+        cls().run()
+
+    @staticmethod
+    def roleopt(ctx, param, value: str) -> str:
+        if value not in apps.appslist:
+            raise ValueError(value)
+        return value
+
+    @staticmethod
+    def reload_extra(role: str) -> Iterator[str]:
+        yield f'**/*.py'
+        yield f'logging.yml'
+        if role in ('frontend', 'app'):
+            for ext in 'js css scss jinja'.split():
+                yield f'frontend/src/**/*.{ext}'
 
 if __name__ == '__main__':
-    import uvicorn
-
-    def cmd(*, role: str, **kw):
-        if not hasattr(apps, role):
-            raise ValueError(role) 
-        logger.info(f'Starting uvicorn {role=}')
-        pkgdirname = settings.BASEDIR.name
-        if settings.UVICORN_RELOAD:
-            kw.update(
-                reload_includes=[
-                    f'{pkgdirname}/**/*.py',
-                    f'{pkgdirname}/logging.yml',
-                    *map(
-                        f'{pkgdirname}/''frontend/src/**/*.{}'.format,
-                        'js css scss jinja'.split())])
-        return uvicorn.main.callback(f'{pkgdirname}.main:apps.{role}', **kw)
-    params = [
-        click.Argument(['role'], default='app'),
-        *uvicorn.main.params[1:]]
-    main = click.Command(
-        name='main',
-        callback=cmd,
-        params=params,
-        context_settings=uvicorn.main.context_settings)
-    main()
+    Command.main()
