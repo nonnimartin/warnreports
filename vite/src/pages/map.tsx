9cf9c62623831bfcd430100f8c2a4264dade7ae7
nonnimartin <nonnimartin@gmail.com>
2025-03-21 09:29:37 -0700

    Messing with map

--- vite/src/pages/map.tsx
+++ vite/src/pages/map.tsx
@@ -1,19 +1,24 @@
 import * as React from 'react';
 import Map from 'react-map-gl/maplibre';
 import 'maplibre-gl/dist/maplibre-gl.css';
+import * as maptilersdk from '@maptiler/sdk';
+
+const map = (<Map
+initialViewState={{
+  longitude: -90.2446,
+  latitude: 38.12924,
+  zoom: 3
+}}
+style={{width: 600, height: 400}}
+mapStyle="https://api.maptiler.com/maps/0195b4e1-ab90-7ad0-9fb2-445919d31f36/style.json?key=ADifuwf2XQ4HVeGoLsWP"
+/>);
+
+const marker = new maptilersdk.Marker()
+  .setLngLat([30.5, 50.5])
+  .addTo(map);
 
 function App() {
-  return (
-    <Map
-      initialViewState={{
-        longitude: -90.2446,
-        latitude: 38.12924,
-        zoom: 3
-      }}
-      style={{width: 600, height: 400}}
-      mapStyle="https://api.maptiler.com/maps/0195b4e1-ab90-7ad0-9fb2-445919d31f36/style.json?key=ADifuwf2XQ4HVeGoLsWP"
-    />
-  );
+  return (map);
 }
 
 export default App
