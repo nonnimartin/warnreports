b7e4bf73f8fd824044dc79f3a95682659b2fca82
nonnimartin <nonnimartin@gmail.com>
2025-03-22 16:53:55 -0700

    Started getting coordinates for markers

--- vite/src/pages/map.tsx
+++ vite/src/pages/map.tsx
@@ -4,13 +4,15 @@ import { fetchok } from '../lib/utils'
 import * as maptilersdk from '@maptiler/sdk';
 import "@maptiler/sdk/dist/maptiler-sdk.css";
 
-export async function clientLoader({ params }: Route.LoaderArgs) {
-  const rep = await fetchok(`/api/v0/reports/${params.id}`)
-  const report = await rep.json()
+export async function clientLoader() {
+  const rep = await fetchok(`/api/v0/reports?offset=0&limit=25&order=-reported`);
+  const report = await rep.json();
   return { report }
 }
 
-export default function Map() {
+export default function Map({loaderData}) {
+  const { report } = loaderData
+  console.log(report);
   const mapContainer = useRef(null);
   const map = useRef(null);
   const usa = { lng: -95.2446, lat: 38.12924 };
@@ -25,10 +27,18 @@ export default function Map() {
       center: [usa.lng, usa.lat],
       zoom: zoom
     });
-    // Adding a marker
-    const marker = new maptilersdk.Marker()
-      .setLngLat([30.5, 50.5])
-      .addTo(map.current);
+
+    report.forEach((thisReport) => {
+
+      // make a request to https://api.maptiler.com/geocoding/<CITY>.json?key=<API_KEY>
+      // for coordinates
+
+
+      //Adding a marker
+        let marker = new maptilersdk.Marker()
+          .setLngLat([30.5, 50.5])
+          .addTo(map.current);
+    });
 
   }, [usa.lng, usa.lat, zoom]);
 
