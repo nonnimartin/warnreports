f16b2fd046fc90b333eaa723445456adb1b1e20a
nonnimartin <nonnimartin@gmail.com>
2025-03-23 15:17:23 -0700

    Working on render logic

--- vite/src/pages/map.tsx
+++ vite/src/pages/map.tsx
@@ -6,13 +6,44 @@ import "@maptiler/sdk/dist/maptiler-sdk.css";
 
 export async function clientLoader() {
   const rep = await fetchok(`/api/v0/reports?offset=0&limit=25&order=-reported`);
-  const report = await rep.json();
-  return { report }
+  const reports = await rep.json();
+  let queryString = '';
+  let addedLocations = [];
+  for (const report in reports){
+    let reportObj = reports[report];
+    console.log(reportObj);
+    if (reportObj.location == null){ continue }
+    console.log(reportObj.location);
+    if (!addedLocations.includes(reportObj.location)) queryString += reportObj.location + ';';
+    console.log(addedLocations);
+    addedLocations.push(reportObj.location);
+  }
+  console.log(queryString);
+  let coordsMap = await fetchok(`https://api.maptiler.com/geocoding/` + queryString + `.json?key=ADifuwf2XQ4HVeGoLsWP&country=us`);
+  let locationMap = {};
+  console.log('json');
+  console.log(coordsMap);
+  coordsMap.json().then((response) => {
+    for (const location in response){
+      let thisLocation = response[location];
+      console.log(thisLocation);
+      if (thisLocation['features'].length == 0) continue;
+      if (thisLocation['features'][0].length == 0) continue;
+      let longitude = thisLocation['features'][0]['geometry']['coordinates'][0];
+      console.log(longitude);
+      let latitude = thisLocation['features'][0]['geometry']['coordinates'][1];
+      console.log(latitude);
+      let placeName = thisLocation['features'][0]['place_name'];
+      locationMap[placeName] = {'latitude':latitude, 'longitude':longitude};
+    }
+    console.log(locationMap);
+    reports['coordsMap'] = locationMap;
+  });
+  return reports;
 }
 
 export default function Map({loaderData}) {
-  const { report } = loaderData
-  console.log(report);
+  const { reports } = loaderData ?? {}
   const mapContainer = useRef(null);
   const map = useRef(null);
   const usa = { lng: -95.2446, lat: 38.12924 };
@@ -27,18 +58,16 @@ export default function Map({loaderData}) {
       center: [usa.lng, usa.lat],
       zoom: zoom
     });
+    console.log(reports);
+    for (const report of reports)((thisReport) => {
+    console.log('this report');
+    console.log(thisReport);
 
-    report.forEach((thisReport) => {
-
-      // make a request to https://api.maptiler.com/geocoding/<CITY>.json?key=<API_KEY>
-      // for coordinates
-
-
-      //Adding a marker
-        let marker = new maptilersdk.Marker()
-          .setLngLat([30.5, 50.5])
-          .addTo(map.current);
-    });
+    //Adding a marker
+      let marker = new maptilersdk.Marker()
+        .setLngLat([30.5, 50.5])
+        .addTo(map.current);
+  });
 
   }, [usa.lng, usa.lat, zoom]);
 
